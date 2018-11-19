f966597cb4c7fa892639e3a9f9129a707a44cfa8
lamost <lamost710@126.com>
2018-11-19 13:10:54 +0800

    123

---
+++ my-springboot-demo/dao/src/main/java/config/dataSourceConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class dataSourceConfig {
+    @Bean
+    public ComboPooledDataSource createdatasource(){
+    }
+}
